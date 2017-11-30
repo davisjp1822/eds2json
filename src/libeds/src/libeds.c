1debd1673f75f5cd2b204024ad76aa3a5e15a7c8
John Davis <jd@three-ml.com>
2017-11-30 13:40:10 -0500

    initial commit

---
+++ src/libeds/src/libeds.c
@@ -0,0 +1,33 @@
+/**
+
+	@file libeds.c
+	@author John Davis <jd@three-ml.com>
+	@date 30 Nov 2017
+	@brief libeds - an open source ODVA EDS file parser.
+
+	Copyright 2017 3ML LLC (www.three-ml.com)
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+	 http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+
+**/
+
+#include "libeds.h"
+
+#include <stdio.h>
+#include <stdint.h>
+
+int8_t main(int8_t argc, char **argv)
+{
+	fprintf(stderr, "%s\n", "Hello world!");
+	return 0;
+}
