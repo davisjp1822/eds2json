bde2018063662dd839c0392a85d70e81ab1f3a09
John Davis <jd@three-ml.com>
2018-01-01 14:32:13 -0500

    simple sample program working

--- src/examples/src/simple_example.c
+++ src/examples/src/simple_example.c
@@ -26,9 +26,163 @@
 
 #include <stdio.h>
 #include <stdint.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+void print_usage();
 
 int8_t main(int8_t argc, char **argv)
 {
-	fprintf(stderr, "%s\n", "Hello world from eds2json!");
+
+	int8_t opt = 0;
+	char *eds_file_path = NULL;
+	char *output_json_file_path = NULL;
+	bool print_json_to_stdout = false;
+
+	while ((opt = getopt(argc, argv, "hf:so:")) != -1)
+	{
+		switch(opt)
+		{
+			case('f'):
+			{
+				eds_file_path = optarg;
+				break;
+			}
+
+			case('s'):
+			{
+				print_json_to_stdout = true;
+				break;
+			}
+
+			case('o'):
+			{
+				output_json_file_path = optarg;
+				break;
+			}
+
+			case('h'):
+			{
+				print_usage();
+				return 0;
+				break;
+			}
+
+			case('?'):
+			{	
+				if(optopt == 'f' || optopt == 'o')
+				{
+					fprintf(stderr, "Option %c requires an argument\n", optopt);
+				}
+
+				else
+				{
+					print_usage();
+				}
+
+				return 1;
+				break;
+			}
+
+			default:
+			{
+				exit(-1);
+			}
+		}
+	}
+
+	// no arguments! print usage and exit. 
+	if(argc == 1)
+	{
+		print_usage();
+		return 1;
+	}
+
+	// must specify a file path!
+	if(!eds_file_path)
+	{
+		fprintf(stderr, "%s", "You must specify a path to a valid EDS file!\n");
+		return 1;
+	}
+
+	// now the good stuff
+	char json_output[LARGE_BUF] = {0};
+	size_t output_json_chars = 0;
+
+	ERR_LIBEDS_t err = convert_eds2json(eds_file_path, json_output, sizeof(json_output), &output_json_chars);
+
+	if(err != 0)
+	{
+
+		// 128 is the minimum length for error buffers
+		char e_string[128] = {0};
+
+		int8_t r = err_string(err, e_string, sizeof(e_string));
+		
+		if(r==0)
+		{
+			fprintf(stderr, "Fatal EDS Parsing Error: %s\n", e_string);
+		}
+		else
+		{
+			fprintf(stderr, "Fatal EDS Parsing Error: %s\n", "Unknown error and also error parsing libeds2json error code!");
+		}
+
+		return 1;
+	}
+
+	else
+	{
+		if(print_json_to_stdout)
+		{
+			fprintf(stdout, "{%s}\n", json_output);
+			fflush(stdout);
+		}
+
+		if(output_json_file_path)
+		{
+			FILE *f = fopen(output_json_file_path, "w");
+
+			if(f)
+			{
+				size_t fbuf_size = strlen(json_output)+4;
+				char fbuf[fbuf_size];
+
+				memset(fbuf, 0, fbuf_size);
+				snprintf(fbuf, fbuf_size, "{%s}\n", json_output);
+
+
+				fwrite(fbuf, fbuf_size-1, 1, f);
+				fclose(f);
+				return 0;
+			}
+			else
+			{
+				fprintf(stderr, "Fatal EDS Parsing Error: %s\n", strerror(errno));
+				return 1;
+			}
+		}
+	}
+
 	return 0;
+}
+
+void print_usage()
+{
+	const char *s = "\nlibeds2json Simple Test Program\n"
+					"A Three Martini Lunch (ThreeML) production, vintage 2018\n"
+					"Usage:\n"
+					"------\n"
+					"-f <path to EDS file to get JSON-ified>\n"
+					"-s prints JSON to stdout\n"
+					"-o <path to output file> prints JSON to file at specified path\n"
+					"-h prints this message\n"
+					"\n"
+					"-f is always required\n"
+					"If -o is not specified, this program defaults to printing the data to stdout\n\n";
+
+	fprintf(stderr, "%s", s);
 }
